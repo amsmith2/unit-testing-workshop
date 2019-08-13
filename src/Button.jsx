1300853dc2429b8876bac65a9fb77387d36a1c50
amsmith2 <abby.smith@sky.uk>
2019-08-13 09:54:28 +0100

    refactor button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const Button = ({onClick, name, text}) => (
-  <button name={name} onClick={onClick} type="button">{text}</button>
-)
+  <button name={name} onClick={onClick} type="button" data-testid={name}>
+    {text}
+  </button>
+);
